164297de559ca5a245c977bd97017c863d87aae8
Bjarki Madsen <bjerkins8@gmail.com>
2015-11-17 18:06:33 +0100

    adding bootstrap

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sass = require('node-sass-middleware');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var assignments = require('./routes/assignments');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// use sass
+app.use(sass({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'expanded'
+}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/assignments', assignments);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
