383a7523183859508ad81d7646c6b15bf7392f43
Bjarki Madsen <bjerkins8@gmail.com>
2015-11-17 21:38:13 +0100

    adding title

--- routes/assignments.js
+++ routes/assignments.js
@@ -4,7 +4,9 @@ var router = express.Router();
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
   if (id == 1) {
-    res.render('assignment_one');
+    res.render('assignment_one', {
+      title: 'Assignment 1'
+    });
   } else {
     res.render('error', {
       message: 'Coming soon!'
