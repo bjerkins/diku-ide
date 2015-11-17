ccfc79ceddd1d30a06401e5f6620bb0af13fd4d7
Bjarki Madsen <bjerkins8@gmail.com>
2015-11-17 21:04:44 +0100

    adding assignment 1

--- routes/assignments.js
+++ routes/assignments.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('here be assignments');
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  if (id == 1) {
+    res.render('assignment_one');
+  } else {
+    res.render('error', {
+      message: 'Coming soon!'
+    });
+  }
 });
 
 module.exports = router;
