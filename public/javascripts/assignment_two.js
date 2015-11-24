38580389df85d53b45558d9bef53fc2fbf7daee3
Bjarki Madsen <bjerkins8@gmail.com>
2015-11-24 17:57:47 +0100

    adding csv and loading it using d3

--- public/javascripts/assignment_two.js
+++ public/javascripts/assignment_two.js
@@ -1,4 +1,11 @@
 
-// script yer assignment two here, yarr
+// assignment two!
 
-console.log('assignment 2 ready to pounce');
+
+d3.csv('/javascripts/akureyri.csv', function (data) {
+  init(data);
+});
+
+function init(data) {
+  console.log('wee, lots of data', data);
+};
