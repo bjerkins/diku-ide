8ddd469f4f1821fe00622301ec5c3498665d9b81
siozosdr <sokras2008@gmail.com>
2015-11-30 10:10:31 +0100

    syntax error

--- routes/assignments.js
+++ routes/assignments.js
@@ -11,10 +11,11 @@ router.get('/:id', function(req, res, next) {
     res.render('assignment_two', {
       title: 'Assignment 2'
     });
-  } else if (id == 3) {
-    res.render('assignment_three', {
-      title: 'Assignment 3'
-  }); else {
+  } else if (id == 2) {
+    res.render('assignment_two', {
+      title: 'Assignment 2'
+    });
+  } else {
     res.render('error', {
       message: 'Coming soon!'
     });
