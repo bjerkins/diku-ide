fc280f9fe0ea799e71ce588d42201cb5e37345ed
Bjarki Madsen <bjerkins8@gmail.com>
2015-12-05 16:05:08 +0100

    adding axis

--- public/javascripts/assignment_three/draw-hands.js
+++ public/javascripts/assignment_three/draw-hands.js
@@ -1,23 +1,67 @@
 var DrawHands = {
 
-  draw: function (id, data) {
+  draw: function (id, handData) {
 
-    var scale = 500;
+    var scale = 500,
+        height = 500,
+        width = 500,
+        padding = 30;
 
+    // scale functions
+    var xScale = d3.scale.linear()
+      .domain([0, d3.max(handData, function(d) { return d.x; })])
+      .range([padding, width - padding * 2]);
+
+    var yScale = d3.scale.linear()
+      .domain([0, d3.max(handData, function(d) { return d.y; })])
+      .range([height - padding, padding]);
+
+    // create x axis
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom")
+      .ticks(5);
+
+    // create y axis
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("left")
+      .ticks(5);
+
+    // create our line function
     var lineFn = d3.svg.line()
-      .x(function(d) { return d.x * scale; })
-      .y(function(d) { return d.y * scale; })
+      .x(function(d) { return xScale(d.x) * scale; })
+      .y(function(d) { return yScale(d.y) * scale; })
       .interpolate("basis");
 
-    var hand = d3.select(id)
-      .attr("width", scale)
-      .attr("height", scale);
+    // create our hand container
+    var container = d3.select(id)
+      .attr("width", width)
+      .attr("height", height);
     
-    hand.append("path")
-      .attr("d", lineFn(data))
+    //
+    // draw lines
+    //
+
+    container.append("path")
+      .attr("d", lineFn(handData))
       .attr("stroke", d3.scale.category10(1))
       .attr("stroke-width", 2)
       .attr("fill", "none");
+
+    //
+    // append axis
+    //
+
+    container.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + (height - padding) + ")")
+      .call(xAxis);
+      
+    container.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + padding + ",0)")
+      .call(yAxis);
   }
-  
+
 };
