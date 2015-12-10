2777f6a784db7a2901004f8b2ea2a76e9165c23b
Bjarki Madsen <bjerkins8@gmail.com>
2015-12-10 14:35:43 +0100

    drawing map of san fran

--- public/javascripts/assignment_four/assignment_four.js
+++ public/javascripts/assignment_four/assignment_four.js
@@ -1,30 +1,48 @@
 // script for assignment four
 
-    var map = new Datamap({
-        element: document.getElementById('map1'),
-        scope: 'usa',
-        fills: {
-            HIGH: '#afafaf',
-            LOW: '#123456',
-            MEDIUM: 'blue',
-            UNKNOWN: 'rgb(0,0,0)',
-            defaultFill: 'green'
-        },
-        data: {
-            CA: {
-                fillKey: 'LOW',
-                numberOfThings: 2002
-            },
-            DE: {
-                fillKey: 'MEDIUM',
-                numberOfThings: 10381
-            },
-            NY: {
-                fillKey: 'HIGH',
-                numberOfThings: 10381
-            }
-        }
-    });
+var data;
 
-    //draw a legend for this map
-    map.legend();
+
+d3.json('/javascripts/assignment_four/san_fran.geojson', function (d) {
+    data = d;
+    init();
+});
+
+function init() {
+    var width = 600, 
+        height = 600;
+
+    var svg = d3.select('#map1')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    var projection = d3.geo.mercator()
+        .scale(1)
+        .translate([0, 0])
+        .precision(0);
+
+    var path = d3.geo.path().projection(projection);
+    var bounds = path.bounds(data);
+
+    var xScale = width / Math.abs(bounds[1][0] - bounds[0][0]);
+    var yScale = height / Math.abs(bounds[1][1] - bounds[0][1]);
+    var scale = xScale < yScale ? xScale : yScale;
+
+    var transl = [(width - scale * (bounds[1][0] + bounds[0][0])) / 2, (height - scale * (bounds[1][1] + bounds[0][1])) / 2];
+    projection.scale(scale).translate(transl);
+
+    svg.selectAll('path')
+        .data(data.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('data-id', function(d) {
+            return d.id;
+        })
+        .attr('data-name', function(d) {
+            return d.properties.name;
+        })
+        .style('fill', '#FB5B1F')
+        .style('stroke', '#ffffff');
+};
