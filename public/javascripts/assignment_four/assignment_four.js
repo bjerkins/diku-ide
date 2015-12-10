87d85b7e6ed7888e044b3d6dfceebdf4a76ea49a
siozosdr <sokras2008@gmail.com>
2015-12-10 11:32:13 +0100

    some testing with the map

--- public/javascripts/assignment_four/assignment_four.js
+++ public/javascripts/assignment_four/assignment_four.js
@@ -1,6 +1,30 @@
 // script for assignment four
 
-  var map = new Datamap({
-  	  element: document.getElementById('map1'),
-      scope: 'usa'
-  });
+    var map = new Datamap({
+        element: document.getElementById('map1'),
+        scope: 'usa',
+        fills: {
+            HIGH: '#afafaf',
+            LOW: '#123456',
+            MEDIUM: 'blue',
+            UNKNOWN: 'rgb(0,0,0)',
+            defaultFill: 'green'
+        },
+        data: {
+            CA: {
+                fillKey: 'LOW',
+                numberOfThings: 2002
+            },
+            DE: {
+                fillKey: 'MEDIUM',
+                numberOfThings: 10381
+            },
+            NY: {
+                fillKey: 'HIGH',
+                numberOfThings: 10381
+            }
+        }
+    });
+
+    //draw a legend for this map
+    map.legend();
