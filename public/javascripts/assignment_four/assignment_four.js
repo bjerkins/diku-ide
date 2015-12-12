04294e3d6709fb830c801ad9ee0764d4619836c7
Bjarki Madsen <bjerkins8@gmail.com>
2015-12-12 12:37:52 +0100

    showing map with cartodb

--- public/javascripts/assignment_four/assignment_four.js
+++ public/javascripts/assignment_four/assignment_four.js
@@ -2,4 +2,4 @@
 
 // var san_map = SanFranMap('#map1');
 
-cartodb.createVis('map1', 'https://bjerkins8.cartodb.com/api/v2/viz/0c80d9fc-9f57-11e5-900f-0e8c56e2ffdb/viz.json');
+cartodb.createVis('map1', 'https://bjerkins8.cartodb.com/api/v2/viz/3b39146c-a0c4-11e5-b140-0e787de82d45/viz.json');
