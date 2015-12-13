d23c4a2b2583d2a18833e36cf478f0b68618ae88
siozosdr <sokras2008@gmail.com>
2015-12-13 21:17:10 +0100

    crimes per period of the year

--- public/javascripts/assignment_four/assignment_four.js
+++ public/javascripts/assignment_four/assignment_four.js
@@ -4,3 +4,4 @@
 
 cartodb.createVis('map1', 'https://bjerkins8.cartodb.com/api/v2/viz/3b39146c-a0c4-11e5-b140-0e787de82d45/viz.json');
 cartodb.createVis('map2', 'https://bjerkins8.cartodb.com/api/v2/viz/60185b28-a1b2-11e5-abf0-0ecfd53eb7d3/viz.json');
+cartodb.createVis('map3', 'https://bjerkins8.cartodb.com/api/v2/viz/62947740-a1d2-11e5-8d77-0e3ff518bd15/viz.json');
