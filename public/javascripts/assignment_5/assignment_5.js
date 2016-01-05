33b4f240e6f5321f791e7830c217f98896dacfe5
Bjarki Madsen <bjerkins8@gmail.com>
2016-01-05 20:26:46 +0100

    calculating bonds between atoms

--- public/javascripts/assignment_5/assignment_5.js
+++ public/javascripts/assignment_5/assignment_5.js
@@ -1,9 +1,17 @@
 // script for assignment five
 
-var data, camera, scene, renderer;
+var data, camera, scene, renderer, bonds = {};
 
-d3.csv('/javascripts/assignment_5/data/atoms.csv', function (d) {
-    data = d;
+d3.csv('/javascripts/assignment_5/data/atoms.csv', function(d) {
+  return {
+    id: +d.id,
+    x:  +d.x,
+    y:  +d.y,
+    z:  +d.z,
+    element: d.element
+  };
+}, function(error, rows) {
+    data = rows;
     init();
     animate();
 });
@@ -26,6 +34,8 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
+
+    findBonds();
 }
 
 function animate() {
@@ -35,4 +45,32 @@ function animate() {
     mesh.rotation.y += 0.02;
 
     renderer.render(scene, camera);
+}
+
+function findBonds() {
+    make_map();
+    for (var i = 0; i < data.length; i++) {
+        for (var j = i+1; j < data.length; j++) {
+            var atom_one = data[i];
+            var atom_two = data[j];
+            if (atom_one.id !== atom_two.id && checkBond(atom_one, atom_two) && !_.contains(bonds[atom_two], atom_one.id)) {
+                bonds[atom_one.id].push(atom_two.id);
+            }
+        }
+    }
+}
+
+function make_map() {
+    data.forEach(function (d) {
+        bonds[d.id] = [];
+    });
+}
+
+function checkBond(atom_one, atom_two) {
+	var distance = Math.sqrt(
+        Math.pow((atom_one.x - atom_two.x), 2) + 
+        Math.pow((atom_one.y - atom_two.y), 2) + 
+        Math.pow((atom_one.z - atom_two.z), 2)
+    );
+    return distance < 1.9;
 }
