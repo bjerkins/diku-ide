85a548061eb9534bf319d77c342251296ebc7197
Bjarki Madsen <bjerkins8@gmail.com>
2016-01-07 22:57:27 +0100

    trying the cylinder

--- public/javascripts/assignment_5/assignment_5.js
+++ public/javascripts/assignment_5/assignment_5.js
@@ -18,7 +18,7 @@ d3.csv('/javascripts/assignment_5/data/atoms.csv', function(d) {
     element: d.element
   };
 }, function(error, rows) {
-    data = rows;
+    data = rows.slice(0, 100);
     init();
     renderScene();
 });
@@ -26,23 +26,18 @@ d3.csv('/javascripts/assignment_5/data/atoms.csv', function(d) {
 
 // expects 'data' to have been initialized
 function init() {
-    // set up clock
+    findBonds();
+    
     clock = new THREE.Clock();
 
 	scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.x = -20;
-    camera.position.z = 60;
+    camera.position.x = -20 * CONST;
+    camera.position.z = 60 * CONST;
 
-    data.forEach(function (atom) {
-        var material = new THREE.MeshBasicMaterial({
-            color: atomColor(atom.element)
-        });
-        var mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.5, 5, 5 ), material);
-        mesh.position.set(atom.x * CONST, atom.y * CONST, atom.z * CONST);
-        scene.add(mesh);
-    });
+    drawAtoms();
+    drawBonds();
 
     // set up camera fly controls (W A S D etc. + arrows)
     flyControls = new THREE.FlyControls(camera);
@@ -56,12 +51,34 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
+}
 
-    findBonds();
+function drawAtoms() {
+    // draw atoms
+    data.forEach(function (atom) {
+        var material = new THREE.MeshBasicMaterial({ color: atomColor(atom.element) });
+        var mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.5, 8, 6 ), material);
+        mesh.position.set(atom.x * CONST, atom.y * CONST, atom.z * CONST);
+        scene.add(mesh);
+    });
+}
+
+function drawBonds() {
+    for (var atom_id in bonds) {
+        var origin = bonds[atom_id];
+        origin.bonds.forEach(function (bonded_atom) {
+            var material = new THREE.MeshBasicMaterial( {color: 'green'} );
+            var geometry = new THREE.CylinderGeometry( 0.5, 0.5, 20, 32 );
+            geometry.vertices.push(bonded_atom.vector);
+            var cylinder = new THREE.Mesh(geometry, material);
+            cylinder.position.set(origin.x, origin.y, origin.z);
+            scene.add(cylinder);    
+        });   
+    }
 }
 
 function renderScene() {
-    flyControls.update( clock.getDelta() );
+    flyControls.update(clock.getDelta());
 
     requestAnimationFrame(renderScene);
 
@@ -84,16 +101,32 @@ function findBonds() {
         for (var j = i+1; j < data.length; j++) {
             var atom_one = data[i];
             var atom_two = data[j];
-            if (atom_one.id !== atom_two.id && checkBond(atom_one, atom_two) && !_.contains(bonds[atom_two], atom_one.id)) {
-                bonds[atom_one.id].push(atom_two.id);
+            var vector = vectorBetweenAtoms(atom_one, atom_two);
+            if (atom_one.id !== atom_two.id && checkBond(atom_one, atom_two) && contains(bonds[atom_two.id].bonds, atom_one.id)) {
+                bonds[atom_one.id].bonds.push({
+                    atomId: atom_two.id,
+                    vector: vector
+                });
             }
         }
     }
 }
 
+function contains(atoms, atom_id) {
+    var matches = _.filter(atoms, function(atom){ 
+        if (atom.atomId === atom_id) { 
+            return atom;
+        } 
+    });
+    return matches;
+}
+
 function make_map() {
     data.forEach(function (d) {
-        bonds[d.id] = [];
+        bonds[d.id] = {
+            atom: d,
+            bonds: []
+        };
     });
 }
 
@@ -104,4 +137,12 @@ function checkBond(atom_one, atom_two) {
         Math.pow((atom_one.z - atom_two.z), 2)
     );
     return distance < 1.9;
+}
+
+function vectorBetweenAtoms(atom_one, atom_two) {
+    return new THREE.Vector3(
+        atom_one.x - atom_two.x,
+        atom_one.y - atom_two.y,
+        atom_one.z - atom_two.z
+    )
 }
