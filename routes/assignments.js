16f392b5f4dd3980c366f4d8e998ecf7a16460a9
Bjarki Madsen <bjerkins8@gmail.com>
2016-01-10 00:17:18 +0100

    adding sub pages and required files

--- routes/assignments.js
+++ routes/assignments.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:id', function(req, res, next) {
-  var id = req.params.id;
+router.get('/:id/:part', function(req, res, next) {
+  var id = req.params.id,
+      part = req.params.part;
   if (id == 1) {
     res.render('assignment_1', {
       title: 'Assignment 1'
@@ -18,6 +19,14 @@ router.get('/:id', function(req, res, next) {
   } else if (id == 4) {
     res.render('assignment_4', {
       title: 'Assignment 4'
+    });
+  } else if (id == 5 && part == 1) {
+    res.render('assignment_5_1', {
+      title: 'Assignment 5, Part 1'
+    });
+  } else if (id == 5 && part == 2) {
+    res.render('assignment_5_2', {
+      title: 'Assignment 5, Part 2'
     });
   } else if (id == 5) {
     res.render('assignment_5', {
