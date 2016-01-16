2503e202eec0f7d0a29a77fe1695c3b4215afd4f
Bjarki Madsen <bjerkins8@gmail.com>
2016-01-16 14:15:00 +0100

    adding the map using datamaps

--- public/javascripts/assignment_6/assignment_6.js
+++ public/javascripts/assignment_6/assignment_6.js
@@ -1,2 +1,15 @@
 
-console.log('let there be code here');
+
+var basic_choropleth = new Datamap({
+  element: document.getElementById("map"),
+  projection: 'mercator',
+  fills: {
+    defaultFill: "#ABDDA4",
+    customFill: "#b000b5"
+  },
+  data: {
+    ISL: { fillKey: "customFill" }
+  }
+});
+    
+
